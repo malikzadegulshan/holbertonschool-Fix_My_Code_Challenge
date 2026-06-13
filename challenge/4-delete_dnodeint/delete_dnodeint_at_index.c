dc34c8355333f0114b783364ac46517fc4e2264c
malikzadegulshan <malikzade.gulshan@gmail.com>
2026-06-13 20:03:02 +0400

    update

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
@@ -11,45 +10,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *saved_head;
-    dlistint_t *tmp;
-    unsigned int p;
-
-    if (*head == NULL)
-        return (-1);
-
-    saved_head = *head;
-    p = 0;
-
-    while (p < index && *head != NULL)
-    {
-        *head = (*head)->next;
-        p++;
-    }
-
-    if (p != index)
-    {
-        *head = saved_head;
-        return (-1);
-    }
-
-    if (index == 0)
-    {
-        tmp = (*head)->next;
-        free(*head);
-        *head = tmp;
-        if (tmp != NULL)
-            tmp->prev = NULL;
-    }
-    else
-    {
-        tmp = *head;
-        tmp->prev->next = tmp->next;
-        if (tmp->next)
-            tmp->next->prev = tmp->prev;
-        free(tmp);
-        *head = saved_head;
-    }
-
-    return (1);
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		tmp = (*head)->next;
+		(*head)->prev->next = tmp;
+		if (tmp)
+			tmp->prev = (*head)->prev;
+		free(*head);
+		*head = saved_head;
+	}
+	return (1);
 }
